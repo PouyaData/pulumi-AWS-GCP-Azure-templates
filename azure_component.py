80b5ea065e0df646bfedf0ebe6e63755986b1341
kAutoman <automan990923@gmail.com>
2025-03-25 00:16:36 -0700

    Update azure_component.py

--- azure_component.py
+++ azure_component.py
@@ -15,58 +15,6 @@ class AzureComponent(CloudServiceComponent):
         # Get config values
         self.config = config        
 
-
-    def create_key_vault(self):
-        # Logic for accessing secrets from Azure Key Vault
-        # key_vault_name = self.config.get("key_vault_name")
-        # subscription_id = self.config.get("subscription_id")
-        # print(subscription_id)
-        # resource_group_name = self.config.get("resource_group_name")
-
-        # key_vault_id = f"/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.KeyVault/vaults/{key_vault_name}"
-
-        # # Get secrets from Key Vault
-        # # admin_username = azure.keyvault.get_secret(name="adminUsername", key_vault_id=key_vault_id).value
-        # # admin_password = azure.keyvault.get_secret(name="adminPassword", key_vault_id=key_vault_id).value
-
-        # self.admin_username = "admin"
-        # self.admin_password = "Super!@#$$#@!1234"
-
-
-    def create_network(self):
-        # Logic for creating a Virtual         
-        # appname = pulumi.get_project()
-        # location = self.config.get("location")
-        # resource_group_name = self.config.get("resource_group_name")
-
-        # virtual_network = azure_native.network.VirtualNetwork("virtualNetwork",
-        #     address_space={
-        #         "addressPrefixes": ["10.0.0.0/16"],
-        #     },
-        #     location=location,
-        #     resource_group_name=resource_group_name,
-        #     virtual_network_name=f"{appname}-azure-vn")
-
-        # subnet = azure_native.network.Subnet("subnet",
-        #     address_prefix="10.0.0.0/16",
-        #     resource_group_name=resource_group_name,
-        #     subnet_name=f"{appname}-azure-sn",
-        #     virtual_network_name=virtual_network.name)
-
-        # self.subnet_id = subnet.id
-
-        # # Logic for creating a Public IP Address
-        # location = self.config.get("location")
-        # resource_group_name = self.config.get("resource_group_name")
-
-        # public_ip = azure_native.network.PublicIPAddress(f"publicIP-azure",
-        #     resource_group_name=resource_group_name,
-        #     location=location,
-        #     public_ip_allocation_method="Dynamic")
-
-        # self.public_ip_id = public_ip.id
-
-    
     def create_instance(self):
         # Import the program's configuration settings
         config = pulumi.Config()
