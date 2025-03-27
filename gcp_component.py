60a0d595d8ab69981893a2f6d30eb02dee185864
kAutoman <automan990923@gmail.com>
2025-03-27 09:47:29 -0700

    Update gcp_component.py

--- gcp_component.py
+++ gcp_component.py
@@ -110,6 +110,7 @@ class GCPComponent(CloudServiceComponent):
         subnet = gcp.compute.Subnetwork(
             "subnet",
             ip_cidr_range="10.0.1.0/24",
+            region="us-central1",
             network=network.id,
         )
         
